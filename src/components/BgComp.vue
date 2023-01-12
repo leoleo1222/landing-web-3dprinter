057976a13eb6c592df89c31d2113f003a2c8ee01
raymond124 <19222432@life.hkbu.edu.hk>
2023-01-13 03:06:15 +0800

    Raymond's part Day2

--- src/components/BgComp.vue
+++ src/components/BgComp.vue
@@ -1,32 +1,71 @@
 <template>
   <div class="container pt-4">
+    <p data-aos="fade-left">
+    The promotion of STEM education was first proposed in the 2015 Policy Address and 
+    further supported in the 2016 Policy Address. Art was soonly added to the program
+     and became STEAM. Thanks to the effort contributed by teachers and education workers, 
+     STEAM has become a popular concept among the education industry. Many schools have 
+     started to implement STEAM education in their curriculum which is a great step forward.
+    </p>
+
+    <p data-aos="fade-right">
+    However, schools had faced difficulties when getting new technology into campus. 
+    The high set up cost, ecosystem set by commercial companies, difficult for deployment etc. 
+    It taken away the treasurable opportunity for students to have a taste of 3D printing. 
+    All these barriers limit schools from getting access to such powerful and convenient technology.
+    </p>
+    
     <div class="textbox">
       <h2 data-text="3D PRINTING">3D PRINTING</h2>
     </div>
 
-    <h5 class="text-white">Thanks to the effort contributed by teachers and education workers, STEAM has become a popular concept among the education industry. Many schools have started to implement STEAM education in their curriculum which is a great step forward.
-</h5>
+    <p>
+      Thanks to the effort contributed by teachers and education workers, STEAM
+      has become a popular concept among the education industry. Many schools
+      have started to implement STEAM education in their curriculum which is a
+      great step forward.
+    </p>
 
-    <h3 class="pt-2 text-white">3D printing solution:</h3>
+    <br /><br />
 
-    <div class="row pt-2">
+    <h3 class="pt-2 text-white">Our 3D printing solution:</h3>
+
+    <div class="row">
       <div class="col-sm-6 col-md-4 text-center">
-        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="fade-up">
-          <h1 >Easy to use</h1>
+        <div
+          class="advantage1 mt-4 p-5 bg-secondary text-white rounded"
+          data-aos="fade-up"
+        >
+          <h1 class="title1">Easy to use</h1>
+          <h1 class="title2"></h1>
         </div>
       </div>
       <div class="col-sm-6 col-md-4 text-center">
-        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="fade-up">
-          <h1>Affordable</h1>
+        <div
+          class="advantage2 mt-4 p-5 bg-secondary text-white rounded"
+          data-aos="fade-up"
+        >
+          <h1 class="title1">Affordable</h1>
+          <h1 class="title2"></h1>
         </div>
       </div>
       <div class="col-sm-6 col-md-4 text-center">
-        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="fade-up">
+        <div
+          class="advantage3 mt-4 p-5 bg-secondary text-white rounded"
+          data-aos="fade-up"
+        >
           <h1>Expendable</h1>
+          <h1 class="title2"></h1>
         </div>
       </div>
     </div>
 
+    <br><br>
+
+    <div class="box pt-4">
+      <h3 class="title1">Some title!</h3>
+      <h3 class="title2">THIS IS COOL</h3>
+    </div>
   </div>
 </template>
 
@@ -58,7 +97,7 @@ export default {
 }
 
 body {
-  background: #161a52;
+  background: rgba(0, 5, 40, 0.4);
 }
 
 .textbox {
@@ -67,14 +106,73 @@ body {
   align-items: center;
 }
 
+.advantage1 {
+  /*Turn the background color deeper when the  mouse over this class.*/
+  background-color: #3f7fa1 !important;
+  font-size: auto;
+}
+
+.advantage1:hover {
+  /*Turn the background color deeper when the  mouse over this class.*/
+  background-color: #145376 !important;
+  font-size: 0;
+}
+
+.advantage2 {
+  /*Turn the background color deeper when the  mouse over this class.*/
+  background-color: #3c4782 !important;
+}
+
+.advantage2:hover {
+  /*Turn the background color deeper when the  mouse over this class.*/
+  background-color: #151b3c !important;
+}
+
+.advantage3 {
+  /*Turn the background color deeper when the  mouse over this class.*/
+  background-color: #c0588e !important;
+}
+
+.advantage3:hover {
+  /*Turn the background color deeper when the  mouse over this class.*/
+  background-color: #8a295b !important;
+}
+
+.box {
+  position: relative;
+  height: 200px;
+  background: #0bf;
+  text-align: center;
+}
+.title1,
+.title2 {
+  transition: 0.3s;
+}
+
+.title2 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /* HIDE SECOND TITLE */
+  opacity: 0;
+}
+
+/* WHEN BOX IS HOVERED SWAP TITLES */
+.box:hover .title1 {
+  opacity: 0;
+}
+.box:hover .title2 {
+  opacity: 1;
+}
+
 h1 {
-  font-size: 3rem;
+  font-size: calc(10px + 1.5vw);
 }
 
 h2 {
   position: relative;
-  font-size: calc(12px + 8vw) ;
-  
+  font-size: calc(12px + 8vw);
   color: #161a52;
   -webkit-text-stroke: 0.3vw #f1b015;
   text-transform: uppercase;
@@ -95,8 +193,8 @@ h2::before {
   animation: animate 2.5s linear infinite;
 }
 
-h5{
-   font-size: calc(4px + 2vw) ;
+h5 {
+  font-size: calc(4px + 2vw);
 }
 
 @keyframes animate {
