e996df5cf063e5d8b1e68faa05c55407dfd18bcb
raymond124 <19222432@life.hkbu.edu.hk>
2023-01-12 13:34:49 +0800

    Raymond's part

--- src/views/CView.vue
+++ src/views/CView.vue
@@ -1,12 +1,32 @@
 <template>
-    <div></div>
+<div></div>
+
+<BgComp/>
 </template>
+
 <script>
+import BgComp from '@/components/BgComp.vue';
 // @ is an alias to /src
 
 export default {
     name: "CView",
     components: {
+        BgComp
     },
 };
 </script>
+
+<style>
+.container{
+  min-height: 100vh;
+  justify-content: flex;
+}
+
+* {
+  font-family: "Poppins", sans-serif;
+}
+
+body {
+  background: #1c5f83;      
+}
+</style>
