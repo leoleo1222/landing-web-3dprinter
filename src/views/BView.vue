615d1f484eeaf649c1cb4f36f2c7822d716a4e7c
LAU Kai Yeung <21214638@hkbuad.local>
2023-01-12 13:29:52 +0800

    coding in about us, 90% complete and bug in carosell
    cannot go to the next image

--- src/views/BView.vue
+++ src/views/BView.vue
@@ -1,12 +1,14 @@
 <template>
-    <div></div>
+    <AboutComp/>
 </template>
 <script>
 // @ is an alias to /src
+import AboutComp from "@/components/AboutComp.vue";
 
 export default {
     name: "BView",
     components: {
+        AboutComp
     },
 };
 </script>
