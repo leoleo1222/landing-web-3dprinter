dc49eeecb1ec0a69c8ad4f0b917898c4ded84960
raymond124 <19222432@life.hkbu.edu.hk>
2023-01-12 18:30:07 +0800

    Update BgComp.vue

--- src/components/BgComp.vue
+++ src/components/BgComp.vue
@@ -4,21 +4,24 @@
       <h2 data-text="3D PRINTING">3D PRINTING</h2>
     </div>
 
-    <h3 class="pt-2 text-white">Barriers when facing 3D printing:</h3>
+    <h5 class="text-white">Thanks to the effort contributed by teachers and education workers, STEAM has become a popular concept among the education industry. Many schools have started to implement STEAM education in their curriculum which is a great step forward.
+</h5>
+
+    <h3 class="pt-2 text-white">3D printing solution:</h3>
 
     <div class="row pt-2">
-      <div class="col-sm-4">
-        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="flip-left">
-          <h1>Easy to use</h1>
+      <div class="col-sm-6 col-md-4 text-center">
+        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="fade-up">
+          <h1 >Easy to use</h1>
         </div>
       </div>
-      <div class="col-sm-4">
-        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="flip-left">
+      <div class="col-sm-6 col-md-4 text-center">
+        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="fade-up">
           <h1>Affordable</h1>
         </div>
       </div>
-      <div class="col-sm-4">
-        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="flip-left">
+      <div class="col-sm-6 col-md-4 text-center">
+        <div class="mt-4 p-5 bg-secondary text-white rounded" data-aos="fade-up">
           <h1>Expendable</h1>
         </div>
       </div>
@@ -55,7 +58,7 @@ export default {
 }
 
 body {
-  background: #277ca9;
+  background: #161a52;
 }
 
 .textbox {
@@ -64,10 +67,15 @@ body {
   align-items: center;
 }
 
+h1 {
+  font-size: 3rem;
+}
+
 h2 {
   position: relative;
-  font-size: 8vw;
-  color: #1c5f83;
+  font-size: calc(12px + 8vw) ;
+  
+  color: #161a52;
   -webkit-text-stroke: 0.3vw #f1b015;
   text-transform: uppercase;
   white-space: nowrap;
@@ -85,6 +93,10 @@ h2::before {
   border-right: 2px solid #f1b015;
   overflow: hidden;
   animation: animate 2.5s linear infinite;
+}
+
+h5{
+   font-size: calc(4px + 2vw) ;
 }
 
 @keyframes animate {
