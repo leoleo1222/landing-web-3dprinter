c8c43d89fa64257a2cab856eddc31caefc9a633d
JennyChimHY <chimhoiyan@gmail.com>
2023-01-19 01:33:40 +0800

    Goal updated (unfinished)

--- src/components/GoalComp.vue
+++ src/components/GoalComp.vue
@@ -1,43 +1,89 @@
 <template>
     <meta charset="utf-8">
-    <link href='open-sans.css' rel='stylesheet' type='text/css'>
-
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"-->
+    <!--star
     <div>
         <div class="container">
             <div class="card text-left gap">
                 <div class="card-header gap border-0">
                     <h3 class="goal">Our goals</h3>
-                    <p>     </p>
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                    <p> </p>
                     <p class="fa fa-star goalcontent"></p>
-                    <b class="goalcontent"> All-in-one and easy deployment solution</b> <br/>
+                    <b class="goalcontent"> All-in-one and easy deployment solution</b> <br />
                     <p class="fa fa-star goalcontent"></p>
                     <b class="goalcontent"> Lower the barrier for 3D modeling and printing</b>
                 </div>
+        
+
                 <div class="row">
-                    <div data-aos="flip-left" class="cardgoal goal1">
-                        <h4 class="card-title goal1">Youth</h4>
-                        <p class="goalcontent">Provide an entry point for talented youth</p>
+                    <div data-aos="flip-left" class="col-4">
+                        <div class="col px-3" style="height:100%; width: 100%;">  spacing=0 
+                            <h4 class="card-title goal1">Youth</h4>
+                            <p class="goalcontent">Provide an entry point for talented youth</p>
+                        </div>
                     </div>
 
-                    <!-- card-overlay BUG -->
-                    <div data-aos="flip-left" class="cardgoal goal2">
-                        <h4 class="card-title goal2">Society</h4>
-                        <p class="goalcontent">Open source and free to use</p> 
-                        <p class="goalcontent">Inspire the next generation of innovators and leaders in the Tech industry</p>
+                    <div data-aos="flip-left" class="col-8">
+                        <div class="row" style="background:#ebbfb3; border-radius: 5px;">
+                            <div class="col px-1" style="height:80%; width: 60%;">  spacing=0
+                                <h4 class="card-title goal2">Society</h4>
+                                <p class="goalcontent">Open source and free to use</p>
+                                <p class="goalcontent">Inspire the next generation of innovators and leaders in the Tech
+                                    industry</p>
+                            </div>
+                            <div class="col px-0">
+                                <img src="../assets/goal3.jpg" class="img-fluid rounded-start" alt="Responsive image"
+                                    style=" height: 100%; width: 100%;">
+                            </div>
+                        </div>
                     </div>
+                </div>
 
-                    <div data-aos="flip-left" class="photocontainer goal3">
-                        <img src="../assets/goal3.jpg" class="img-fluid rounded-start" alt="Responsive image" style=" height: 100%; width: 100%;">
-                    </div>
+                <div data-aos="fade-up" class="card-footer border-0">
+                    <p class="text-center slogan">Bringing your ideas to life, layer by layer.</p>
+                </div>
 
-                    <div data-aos="fade-up" class="card-footer border-0">
-                        <p class="text-center slogan">Bringing your ideas to life, layer by layer.</p>
+                <div>
+                    <div class="row expt-holder">
+                        <div class="expt-box" style="width: 100px; height: 100px;"> Box 1 </div>
+                        <div class="expt-box" style="width: 100px; height: 100px;"> Box 2 </div>
+                        <div class="expt-box" style="width: 100px; height: 100px;"> Box 3 </div>
                     </div>
                 </div>
             </div>
         </div>
+
+    </div-->
+
+    <div class="container">
+        <div class="row" style="background: #313aaf">
+            <div class="col-6 py-5 px-5">
+                <h3 class="goal">Project Goals</h3>
+                <p> </p>
+                <p class="goalcontent">Guide you <br/> step-by-step </p>
+
+                <div class="col grid gap-0 row-gap-3">
+                    <img src="../assets/goal3.jpg" class="img-fluid" alt="Responsive image"
+                        style=" height: 100%; width: 100%; max-height: 40vw; max-width: 20vw;">
+                </div>
+            </div>
+
+
+            <div class="col-6 py-5 px-5">
+                <p class="goalcontent">• All-in-one and easy deployment solution</p>
+                <p class="goalcontent">• Lower the barrier for 3D modeling and printing</p>
+                <h4 class="goaltitle">Youth</h4>
+                <p class="goalcontent">• Provide an entry point for talented youth</p>
+                <h4 class="goaltitle">Society</h4>
+                <p class="goalcontent">• Open source and free to use <br/> • Inspire the next generation of innovators and leaders in the Tech
+                    industry</p>
+                <h4 class="goaltitle">School</h4>
+                <p class="goalcontent">• Promote the STEAM education by providing workshop materials</p>
+
+            </div>
+        </div>
     </div>
+
 </template>
 
 
@@ -59,84 +105,48 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap");
 
-* {
-    font-family: "Poppins", sans-serif;
-    font-style: normal;
-    font-weight: 700; /* BUG */
-}
-
-.cardgoal {
+/* .photocontainer {
     width: 100%;
-    height: 100%;
+    height: 90%;
     max-width: 300px;
     max-height: 300px;
-    margin: 1rem;
-    border: 0px;
+    padding: 0;
     border-radius: 5px;
-}
-
-.photocontainer {
-    width:100%;
-    height:90%;
-    max-width: 300px;
-    max-height: 300px;
-    margin: 1rem;
-    padding: 0;    
-    border-radius: 5px;
-}
+    margin-left: 25px;
+} */
 
 .goal {
-    /*font-weight BUG*/
+    font-family: "Poppins", sans-serif;
     position: relative;
     font-weight: normal;
     font-size: 3;
-    color: #420b0b;
+    color: #FFFFFF;
 }
 
-.gap {
-    background: #D78873;
-    position: relative;
-}
-
-.goalabove {
-    font-size: 1.2.5;
-    line-height: 3pt;
-    color: #000000;
-    position: relative;
-}
-
-.goal1 {
-    font-size: 2vw;
-    color: #420b0b;
-    background: #D78873;
-    position: relative;
-}
-
-.goal2 {
-    font-size: 2vw;
-    color: #420b0b;
-    background: #ebbfb3;
-    position: relative;
-}
-
-.goal3 {
-    background: #FFFFFF;
+.goaltitle {
+    font-family: "Poppins", sans-serif;
+    font-size: 1.2vw;
+    font-weight: bold;
+    line-height: 13pt;
+    color: #FFFFFF;
     position: relative;
 }
 
 .goalcontent {
+    font-family: "Poppins", sans-serif;
     font-size: 1.2vw;
     line-height: 13pt;
-    color: #000000;
+    color: #FFFFFF;
     position: relative;
 }
 
-.slogan {
+/* .slogan {
     position: relative;
     font-style: italic;
     font-size: 1.5vw;
     font-weight: 300;
     color: #420b0b;
     background: #ffffff00;
-}
+} */
+
 </style>
