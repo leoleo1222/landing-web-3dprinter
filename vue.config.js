5dbfc6ca088b1f94d7f7dafc0c72c4e4324b0385
raymond124 <19222432@life.hkbu.edu.hk>
2023-01-30 14:02:50 +0800

    Kenny configures some authorization setting

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/~f1214638',
   devServer: {
     proxy: {
       '^/api/.*': {
